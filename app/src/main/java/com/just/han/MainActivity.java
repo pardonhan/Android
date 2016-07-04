65ad050196c423f643158dffb39c3201a3af669a
pardonhan <m864656893@126.com>
2016-07-04 18:04:57 +0800

    RecyclerView

--- app/src/main/java/com/just/han/MainActivity.java
+++ app/src/main/java/com/just/han/MainActivity.java
@@ -1,5 +1,6 @@
 package com.just.han;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,8 +27,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,RecyclerViewActivity.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/just/han/RecyclerViewActivity.java
+++ app/src/main/java/com/just/han/RecyclerViewActivity.java
@@ -1,18 +1,69 @@
 package com.just.han;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Just on 2016/7/4.
- *
  */
-public class RecyclerViewActivity extends AppCompatActivity{
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<String> mDatas;
+    private HomeAdapter mAdapter;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+        initData();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mAdapter = new HomeAdapter());
+    }
 
+    private void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    RecyclerViewActivity.this).inflate(R.layout.activity_recyucle_item, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView tv;
+            public MyViewHolder(View view) {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.id_num);
+            }
+        }
     }
 }

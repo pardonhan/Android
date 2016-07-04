098497a3b50b29137342656f4780165fc1263220
pardonhan <m864656893@126.com>
2016-07-04 17:42:33 +0800

    commit

---
+++ Android自学记录.md
@@ -0,0 +1,7 @@
+# Android 自学记录
+
+	我的Android自学记录。大概记录了我的Android自学过程。
+
+## 环境搭建
+### Java 环境
+### AnroidStudio 环境
---
+++ app/src/main/java/com/just/han/RecyclerViewActivity.java
@@ -0,0 +1,18 @@
+package com.just.han;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Just on 2016/7/4.
+ *
+ */
+public class RecyclerViewActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
